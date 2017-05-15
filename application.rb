e5c9be9a5389ef8edbe1f8fe6109f58e7129d193
Ariel Fogel <fogeltine@gmail.com>
2017-05-15 11:41:07 -0400

    Hard coding is bad. Fix APP_ROOT

--- application.rb
+++ application.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
 
-APP_ROOT = Pathname.new(File.expand_path('../../iArrived', __FILE__))
+APP_ROOT = Pathname.new(File.expand_path('../', __FILE__))
 configure do
   set :root, APP_ROOT.to_path
   set :views, File.join(Sinatra::Application.root, "app", "views")
