b47884fd242ade8a34eae3753b851dc21c3d673d
Ariel Fogel <fogeltine@gmail.com>
2017-05-15 11:14:29 -0400

    add basic layout

--- application.rb
+++ application.rb
@@ -1,5 +1,14 @@
 require 'sinatra'
 
+configure do
+  set :root, APP_ROOT.to_path
+  set :views, File.join(Sinatra::Application.root, "app", "views")
+end
+
 get '/' do
-  "Hello World"
+  "<button style='margin: 80px auto; display: block; width: 50%; height: 50%; font-size: 32px'>I arrived safely!</button>" 
+end
+
+post '/arrived_safely' do
+  flash[:success]
 end
